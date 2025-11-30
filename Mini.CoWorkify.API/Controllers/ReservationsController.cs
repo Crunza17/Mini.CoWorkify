6f06817c0b7afc0059eec28de0911eade24ad16e
Crunza17 <crisyolo.cr17@gmail.com>
2025-11-30 01:55:48 +0100

    ReservationsController created and dependecy injection implemented

---
+++ Mini.CoWorkify.API/Controllers/ReservationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mini.CoWorkify.Application.DTOs;
+using Mini.CoWorkify.Application.Services;
+
+namespace Mini.CoWorkify.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReservationsController : ControllerBase
+{
+    private readonly IReservationService _service;
+    
+    public ReservationsController(IReservationService service)
+    {
+        _service = service;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateReservationDto dto)
+    {
+        try 
+        {
+            var id = await _service.CreateReservationAsync(dto);
+            
+            return Ok(new { Id = id });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { Error = ex.Message });
+        }
+    }
+}
--- Mini.CoWorkify.API/Program.cs
+++ Mini.CoWorkify.API/Program.cs
@@ -1,17 +1,23 @@
+using Mini.CoWorkify.Application.Services;
+using Mini.CoWorkify.Domain.Interfaces;
+using Mini.CoWorkify.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IReservationService, ReservationService>();
+builder.Services.AddSingleton<IReservationRepository, InMemoryReservationRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
